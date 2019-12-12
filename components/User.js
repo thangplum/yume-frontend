2350fb1cd11ab71b1b799ff8df7daf21ef689bc6
Amit Adhikari <end.adhikari@gmail.com>
2019-12-12 13:44:00 +0545

    better error separation

--- components/User.js
+++ components/User.js
@@ -1,5 +1,6 @@
 import gql from "graphql-tag";
 import { useQuery, useApolloClient } from "@apollo/react-hooks";
+import getErrorMessage from "../lib/getErrorMessage";
 
 const CURRENT_USER_QUERY = gql`
   query CURRENT_USER_QUERY {
@@ -15,8 +16,9 @@ const CURRENT_USER_QUERY = gql`
 
 function User(props) {
   const { data, error } = useQuery(CURRENT_USER_QUERY);
-  if (error && error.graphQLErrors) {
-    const { message } = error.graphQLErrors[0];
+
+  if (error) {
+    const message = getErrorMessage(error);
     return props.children({ error: message });
   }
   return props.children({ data });
