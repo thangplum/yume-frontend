ee41cc013c4dd512a75c497ce922380a823093a6
Amit Adhikari <end.adhikari@gmail.com>
2019-12-18 18:22:59 +0545

    refactor header and better error handlig for bad values of category and post slug

--- components/VoteButton.js
+++ components/VoteButton.js
@@ -8,9 +8,9 @@ const ThumbsUpSvg = ({ filled }) => (
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
-    stroke-width="2"
-    stroke-linecap="round"
-    stroke-linejoin="round"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
     className="feather feather-thumbs-up"
     className={"text-yume-red-darker " + (filled ? " fill-current" : "")}
   >
@@ -26,9 +26,9 @@ const ThumbsDownSvg = ({ filled }) => (
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
-    stroke-width="2"
-    stroke-linecap="round"
-    stroke-linejoin="round"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
     className="feather feather-thumbs-down"
     className={"text-yume-red " + (filled ? " fill-current" : "")}
   >
